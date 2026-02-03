87d9d3ece1d5c8a51723517571e0d6bdd548aaef
Bl4nc018 <abr04work@gmail.com>
2026-02-03 12:09:16 +0100

    feat: implementar funcionalidad de búsqueda y mejorar la interfaz de usuario en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
 
   const searchLocation = (event) => {
+    if (event.key !== "Enter") {
     axios.get(url).then((response) => {
       setData(response.data);
       console.log(response.data);
@@ -19,12 +21,52 @@ function App() {
     });
 
     setLocation("");
-  }
+    }
+  };
 
 
   return (
     <div className="app">
-      
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder='Enter Location'
+          type="text"
+        />
+      </div>
+
+      <div className="container"> 
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{Math.round(data.main.temp - 273.15)}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined && (
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? <p className='bold'>{Math.round(data.main.feels_like - 273.15)}°C</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? <p className='bold'>{data.wind.speed} MPH</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
+        )}
+      </div>
     </div>
   )
 }
