ea2df53377e707840ee98bb480077291d7be7bde
Bl4nc018 <abr04work@gmail.com>
2026-02-03 11:31:06 +0100

    feat: actualizado de dependencias y conexión con la API del tiempo en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const API_KEY = "YOUR_API_KEY"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+
+  const searchLocation = (event) => {
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error("Error obteniendo el tiempo:", error);
+    });
+
+    setLocation("");
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      
+    </div>
   )
 }
 
